aaade4e1ea9212814c54595063bf390289451c1e
3liezerDev <3dgar3liezer@gmail.com>
2024-05-20 17:21:51 +0200

    shadcn installation - working

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,10 +1,11 @@
-
+import { Button } from "@/components/ui/button"
 
 export const Home = () => {
     return (
       <>
       <h1 className='text-6xl text-blue-500 text-center'>
           you are in Home
+          <Button className='bg-pink-600 rounded-full '>click me</Button>
       </h1>
       </>
     )
