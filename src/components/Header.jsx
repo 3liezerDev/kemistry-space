dd24e2dd17d0f2f935837a482bd88ce2674df0b5
3liezerDev <3dgar3liezer@gmail.com>
2024-05-20 16:46:49 +0200

    header and overlay dinamic

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,43 +1,25 @@
-import { useLocation } from 'react-router-dom';
+import { useLocation } from "react-router-dom";
 import Navbar from "./Navbar";
+import { Overlay } from "./Overlay";
+import routesConfig from "../routes/routesConfig";
 
 function Header() {
   const location = useLocation();
-  
-  let backgroundImage;
-
-  switch (location.pathname) {
-    case '/about':
-      backgroundImage = 'url("/src/assets/images/Banner-about.jpg")';
-      break;
-    case '/kactivities':
-      backgroundImage = 'url("/src/assets/images/Banner-k-activities.jpg")';
-      break;
-    case '/kevents':
-      backgroundImage = 'url("/src/assets/images/Banner-k-events.jpg")';
-      break;
-    case '/kworkshops':
-      backgroundImage = 'url("/src/assets/images/Banner-k-workshops.jpg")';
-      break;
-    case '/kemistryfestival':
-      backgroundImage = 'url("/src/assets/images/Banner-kemistry-festival.jpg")';
-      break;
-    case '/contact':
-      backgroundImage = 'url("/src/assets/images/Banner-contact.jpg")';
-      break;
-    default:
-      backgroundImage = 'url("/src/assets/images/Banner-home.jpg")';
-      break;
-  }
+  const { image, title, content } = routesConfig[location.pathname] || routesConfig['/']
 
   return (
-    <section className="bg-cover bg-center h-screen" style={{ backgroundImage }}>
+    <>
       <Navbar />
-    </section>
+      <header
+        className="bg-cover bg-center h-screen "
+        style={{ backgroundImage:  `url(${image})` }}
+      >
+        <section className="w-full h-full flex items-center justify-center">
+          <Overlay title={title} content={content}></Overlay>
+        </section>
+      </header>
+    </>
   );
 }
 
 export default Header;
-
-
-
